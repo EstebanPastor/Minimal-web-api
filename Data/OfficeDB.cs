be9e31291ed33c82d87ac327e5f7c8580cf4a4d5
Esteban Pastor <estebanandrespastor@gmail.com>
2023-06-08 03:35:06 -0300

    api finished

---
+++ Data/OfficeDB.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Minimal_Api_PostreqSQLEF.Models;
+
+namespace Minimal_Api_PostreqSQLEF.Data
+{
+    public class OfficeDB : DbContext
+    {
+        public OfficeDB(DbContextOptions<OfficeDB> options) : base(options) 
+        { 
+
+        }
+        public DbSet<Employee> Employees => Set<Employee>();
+
+    }
+}
